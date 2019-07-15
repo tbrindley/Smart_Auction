7b561363321defe30dedbb062e8b42e89f0963c6
tbrindley <travisbrindley@gmail.com>
2019-07-14 22:55:26 -0400

    added AuctionItems model

---
+++ src/main/java/com/ally/model/auctionitem/AuctionItems.java
@@ -0,0 +1,20 @@
+package com.ally.model.auctionitem;
+
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AuctionItems {
+  private Set<AuctionItem> data;
+  private int elapseTimeMillis;
+  private int itemsPerPage;
+  private int page;
+  private int totalItems;
+  private int totalPages;
+}
